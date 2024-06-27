ff7f89a90902c77314bcaf8fd50dd153f49d3855
Rajivsah240 <rajivsah240@gmail.com>
2024-06-27 11:36:26 +0530

    CHAT Improved

--- server.js
+++ server.js
@@ -31,61 +31,64 @@ mongoose
     console.error("Error connecting to MongoDB:", err);
   });
 
-const messageSchema = new mongoose.Schema({
-  group_id: String,
-  user_email: String,
-  username: String,
-  message: String,
-  timestamp: { type: Date, default: Date.now },
-});
-
-const Message = mongoose.model("Message", messageSchema);
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  socket.on("joinGroup", (data) => {
-    const groupId = data.groupId;
-    const userEmail = data.userEmail;
-    socket.join(groupId);
-    io.to(groupId).emit("userJoined", { userEmail: userEmail });
+  const messageSchema = new mongoose.Schema({
+    group_id: String,
+    user_email: String,
+    username: String,
+    message: String,
+    timestamp: { type: Date, default: Date.now },
   });
+  
+  const Message = mongoose.model('Message', messageSchema);
+  
 
-  socket.on("message", (data) => {
-    const groupId = data.groupId;
-    const userEmail = data.userEmail;
-    const username = data.username;
-    const messageText = data.message;
-
-    const messageData = new Message({
-      group_id: groupId,
-      user_email: userEmail,
-      username: username,
-      message: messageText,
+  io.on('connection', (socket) => { 
+    socket.on('joinGroup', (data) => {
+      const groupId = data.groupId;
+      const userEmail = data.userEmail;
+      socket.join(groupId);
+      io.to(groupId).emit('userJoined', { userEmail: userEmail });
     });
-
-    messageData
-      .save()
-      .then(() => {
-        io.to(groupId).emit("newMessage", {
-          userEmail: userEmail,
-          username: username,
-          message: messageText,
-        });
-      })
-      .catch((err) => {
-        console.error("Error saving message:", err);
+  
+    socket.on('message', (data) => {
+      const groupId = data.groupId;
+      const userEmail = data.userEmail;
+      const username = data.username;
+      const messageText = data.message;
+      const timestamp = data.timestamp;
+  
+      const messageData = new Message({
+        group_id: groupId,
+        user_email: userEmail,
+        username: username,
+        message: messageText,
+        timestamp: timestamp,
       });
+  
+      messageData
+        .save()
+        .then(() => {
+          io.to(groupId).emit('newMessage', {
+            userEmail: userEmail,
+            username: username,
+            message: messageText,
+            timestamp: timestamp,
+          });
+        })
+        .catch((err) => {
+          console.error('Error saving message:', err);
+        });
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
   });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
-
-const PORT = process.env.PORT || 5001;
-
-http.listen(PORT, () => { 
+  
+  const PORT = process.env.PORT || 5001;
+  
+  http.listen(PORT, () => {
     const address = http.address();
     console.log(`App listening on ${address.address}:${address.port}`);
-});
+  });
+  
